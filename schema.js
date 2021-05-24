e4cfe646efff7a84e5c4807620a090eb53afd485
yesimnoh <yesimnoh@gmail.com>
2021-05-25 02:28:31 +0900

    delete... 그리고 주석...

--- schema.js
+++ schema.js
@@ -1,8 +1,10 @@
 import { loadFilesSync, mergeTypeDefs, mergeResolvers, makeExecutableSchema } from 'graphql-tools';
 
+// 모든 typeDefs와 Resolver를 하나로 묶어서 가져올 수 있도록 함
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(`${__dirname}/**/*.{queries, mutations}.js`);
 
+// Root typeDefs, resolvers를 만드는 작업
 const typeDefs = mergeTypeDefs(loadedTypes);
 const resolvers = mergeResolvers(loadedResolvers);
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// .env를 활성화 할 수 있도록 dotenv를 설치하여 실행
 require('dotenv').config();
 import { ApolloServer } from 'apollo-server';
 import schema from './schema';
@@ -6,6 +7,7 @@ const server = new ApolloServer({
 	schema,
 });
 
+// .env에서 포트번호 바꾼거 적용
 const PORT = process.env.PORT;
 
 server.listen(PORT).then(() => console.log(`👑 Server is running on http://localhost:${PORT}/`));

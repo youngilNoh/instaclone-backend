b0402271ec783aa6dd2db8efd145f52a26d26003
yesimnoh <yesimnoh@gmail.com>
2021-05-25 02:17:03 +0900

    set env, make architecture

--- server.js
+++ server.js
@@ -1,41 +1,11 @@
-import { ApolloServer, gql } from 'apollo-server';
-
-const typeDefs = gql`
-	type Movie {
-		id: Int
-		title: String
-		year: Int
-	}
-	type Query {
-		movies: [Movie]
-		movie: Movie
-	}
-	type Mutation {
-		createMovie(title: String!): Boolean
-		deleteMovie(title: String!): Boolean
-	}
-`;
-
-const resolvers = {
-	Query: {
-		movies: () => [],
-		movie: () => ({ title: 'Hello', year: 2021 }),
-	},
-	Mutation: {
-		createMovie: (_, { title }) => {
-			console.log(title);
-			return true;
-		},
-		deleteMovie: (_, { title }) => {
-			console.log(title);
-			return true;
-		},
-	},
-};
+require('dotenv').config();
+import { ApolloServer } from 'apollo-server';
+import schema from './schema';
 
 const server = new ApolloServer({
-	typeDefs,
-	resolvers,
+	schema,
 });
 
-server.listen().then(() => console.log('Server is running on http://localhost:4000/'));
+const PORT = process.env.PORT;
+
+server.listen(PORT).then(() => console.log(`👑 Server is running on http://localhost:${PORT}/`));

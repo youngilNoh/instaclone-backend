44e45f76133fc56a043e35c18e3090ea0d446f51
yesimnoh <yesimnoh@gmail.com>
2021-08-28 02:31:02 +0900

    Comment on Photo

---
+++ src/comments/createComment/createComment.resolvers.ts
@@ -0,0 +1,49 @@
+import client from '../../client';
+import { Resolvers } from '../../types';
+import { protectedResolver } from '../../users/users.utils';
+
+const resolversFn = async (_, { photoId, payload }, { loggedInUser }) => {
+	const ok = await client.photo.findUnique({
+		where: {
+			id: photoId,
+		},
+		select: {
+			id: true,
+		},
+	});
+
+	if (!ok) {
+		return {
+			ok: false,
+			error: 'Photo not found.',
+		};
+	}
+
+	await client.comment.create({
+		data: {
+			payload,
+			photo: {
+				connect: {
+					id: photoId,
+				},
+			},
+			user: {
+				connect: {
+					id: loggedInUser.id,
+				},
+			},
+		},
+	});
+
+	return {
+		ok: true,
+	};
+};
+
+const resolvers: Resolvers = {
+	Mutation: {
+		createComment: protectedResolver(resolversFn),
+	},
+};
+
+export default resolvers;
